fd92551ef6e6b69e64742032c50ab9f4303ee129
omchore <omchore8@gmail.com>
2026-07-28 18:26:45 +0530

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/283-MoveZeroes.cpp
@@ -0,0 +1,20 @@
+// Last updated: 7/28/2026, 6:26:45 PM
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+     int j = -1;
+     for(int i=0;i<nums.size();i++){
+        if(nums[i]==0){
+         j=i;
+         break;   
+        }
+     } 
+     if(j == -1) return;
+      for(int i=j+1;i<nums.size();i++){
+        if(nums[i]!=0){
+          swap(nums[i],nums[j]);
+            j++;  
+        }
+     } 
+  }
+};
