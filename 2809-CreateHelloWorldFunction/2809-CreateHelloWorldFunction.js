94952f71f029c6fbff3d2b5555e0e93f2956501a
omchore <omchore8@gmail.com>
2026-07-28 18:26:37 +0530

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,9 @@
+// Last updated: 7/28/2026, 6:26:37 PM
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    return function(...args) {
+        return "Hello World";
+    };
+};
