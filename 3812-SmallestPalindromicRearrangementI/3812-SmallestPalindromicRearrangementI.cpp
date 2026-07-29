553c1f7003362b30b01a31105a55560690350bfb
omchore <omchore8@gmail.com>
2026-07-29 22:17:25 +0530

    Create 3812-SmallestPalindromicRearrangementI

---
+++ 3812-SmallestPalindromicRearrangementI/3812-SmallestPalindromicRearrangementI.cpp
@@ -0,0 +1,28 @@
+// Last updated: 7/29/2026, 10:17:25 PM
+   class Solution {
+public:
+    string smallestPalindrome(string s) {
+        vector<int> freq(26, 0);
+
+        for (char c : s)
+            freq[c - 'a']++;
+
+        string left = "";
+        char mid = '\0';
+
+        for (int i = 0; i < 26; i++) {
+            left += string(freq[i] / 2, 'a' + i);
+
+            if (freq[i] % 2)
+                mid = 'a' + i;
+        }
+
+        string right = left;
+        reverse(right.begin(), right.end());
+
+        if (mid != '\0')
+            return left + mid + right;
+
+        return left + right;
+    }
+};
